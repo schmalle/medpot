0cdd9ee4122661ae667ac734baf55ef9b5153ad5
s9rA16Bf4 <14398606+s9rA16Bf4@users.noreply.github.com>
2022-03-27 20:22:05 +0200

    As you see I might not like docker.

--- docker/step_3.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/env bash
-
-cd /opt/go/src/medpot/template
-mkdir /var/log/medpot
-cp ./medpot.log  /var/log/medpot/medpot.log
-mkdir /data
-mkdir /data/medpot
-cp ./ews.xml /data/medpot/
-cp ./dummyerror.xml /data/medpot/
