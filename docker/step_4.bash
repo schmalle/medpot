b6a78e77aeddd849df64c3b918bf45ab4d36b4b2
s9rA16Bf4 <14398606+s9rA16Bf4@users.noreply.github.com>
2022-03-27 20:15:25 +0200

    Testing 102

--- docker/step_4.bash
+++
@@ -1,5 +0,0 @@
-#!/bin/env bash
-
-addgroup -g 2000 medpot
-adduser -S -s /bin/bash -u 2000 -D -g 2000 medpot
-mkdir -p /var/log/medpot
--- docker/step_5.bash
+++
@@ -1,4 +0,0 @@
-#!/bin/env bash
-
-apk del --purge build-base git go g++
-rm -rf /var/cache/apk/* /opt/go /root/dist
